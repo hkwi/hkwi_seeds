907d8dc2791d23f4085ea28bd17cf620227a5564
Hiroaki KAWAI <hiroaki.kawai@gmail.com>
2013-06-03 15:06:24 +0900

    Add packaging script

---
+++ gevent_openflow/setup.py
@@ -0,0 +1,6 @@
+from distutils.core import setup
+
+setup(name="geventopenflow", version="0.1",
+  description="gevent openflow server/client utils",
+  author="Hiroaki KAWAI", packages=["geventopenflow",])
+
